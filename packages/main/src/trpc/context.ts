fa28c885390310dfcb69f862f405377b26d64769
Nicholai Nissen <nicholainissen@gmail.com>
2022-07-01 13:32:45 +0200

    feat(server/trpc): generalise context

--- packages/main/src/trpc/context.ts
+++ packages/main/src/trpc/context.ts
@@ -1,9 +1,5 @@
 import type * as trpc from '@trpc/server'
-import type * as trpcExpress from '@trpc/server/adapters/express'
 
-export const createContext = ({
-  req,
-  res,
-}: trpcExpress.CreateExpressContextOptions) => ({ req, res }) // no context
+export const createContext = () => ({ }) // no context
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>
