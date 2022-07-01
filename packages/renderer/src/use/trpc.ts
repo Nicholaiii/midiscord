eeb15aba4c378a5fbaa874e9f69a2b182b121d2e
Nicholai Nissen <nicholainissen@gmail.com>
2022-07-01 13:33:49 +0200

    feat(renderer/trpc): add subscriptions over ws

--- packages/renderer/src/use/trpc.ts
+++ packages/renderer/src/use/trpc.ts
@@ -1,13 +1,43 @@
 import type { AppRouter } from '/@main/trpc'
 import { createTRPCClient } from '@trpc/client'
+import { httpLink } from '@trpc/client/links/httpLink'
+import { splitLink } from '@trpc/client/links/splitLink'
+import { createWSClient, wsLink } from '@trpc/client/links/wsLink'
 
-const client = createTRPCClient<AppRouter>({
-  url: 'http://localhost:1312/trpc',
-})
+let persistentClient: ReturnType<typeof createClient>
 
 export const useTRPC = () => {
+  const { client } = init()
+
   return {
     query: client.query.bind(client),
     mutation: client.mutation.bind(client),
+    subscription: client.subscription.bind(client),
   }
+}
+
+function init () {
+  return { client: persistentClient ?? createClient() }
+}
+
+function createClient () {
+  const wsClient = createWSClient({
+    url: 'ws://localhost:1312',
+  })
+
+  const client = createTRPCClient<AppRouter>({
+    links:[
+      splitLink({
+        condition: op => op.type === 'subscription',
+        true: wsLink({
+          client: wsClient,
+        }),
+        false: httpLink({
+          url: 'http://localhost:1312/trpc',
+        }),
+      }),
+    ],
+  })
+
+  return client
 }
