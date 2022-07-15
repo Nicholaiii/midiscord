69205a5764ca733ed15916b9566ede93f4f01d9d
Nicholai Nissen <nicholainissen@gmail.com>
2022-07-15 16:52:09 +0200

    feat(main/config): add subjects for config changes

--- packages/main/src/config/store.ts
+++ packages/main/src/config/store.ts
@@ -1,3 +1,23 @@
+import { BehaviorSubject, Subject } from 'rxjs'
 import { ConfigStore } from '../../../common'
 
 export { ConfigStore }
+
+export const DiscordTokenSubject = new BehaviorSubject<string>(ConfigStore.get('discordToken'))
+
+export type ConfigMsg = {
+  type: 'open'
+}
+
+export const ConfigSubject = new Subject<ConfigMsg>()
+ConfigSubject.subscribe({
+  next(v) {
+    if (v.type === 'open') ConfigStore.openInEditor()
+  },
+})
+
+// trpc
+// DiscordTokenSubject.subscribe(token => ConfigStore.set('discordToken', token))
+
+// fs watch
+// ConfigStore.onDidChange('discordToken', token => token && DiscordTokenSubject.next(token))
