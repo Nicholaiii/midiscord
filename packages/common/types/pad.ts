db06ae3d3524d121a48a01ebc41de0d9283f61d4
Nicholai Nissen <nicholainissen@gmail.com>
2022-08-16 19:08:14 +0200

    feat(common): add colors temporarily untill vuetify publishes typedefs

--- packages/common/types/pad.ts
+++ packages/common/types/pad.ts
@@ -1,4 +1,27 @@
+export type colors =
+| 'red'
+| 'pink'
+| 'purple'
+| 'deep-purple'
+| 'indigo'
+| 'blue'
+| 'light-blue'
+| 'cyan'
+| 'teal'
+| 'green'
+| 'light-green'
+| 'lime'
+| 'yellow'
+| 'amber'
+| 'orange'
+| 'deep-orange'
+| 'brown'
+| 'blue-grey'
+| 'grey'
+| 'shades'
+
 export interface Pad {
   name: string
   file: string
+  color: colors
 }
